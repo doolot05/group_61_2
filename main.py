dcd16a82d93e6c0581d1fb7444295cbaa2a6df8a
doolot05 <doolotesenbekov@gmail.com>
2026-01-13 17:28:40 +0600

    добавил сортировку по алфавиту

--- main.py
+++ main.py
@@ -1,40 +1,69 @@
 import flet as ft
 from datetime import datetime
+
+
 def main(page: ft.Page):
-    page.title = "Мое первое приложение"
-    page.theme_mode = ft.ThemeMode.LIGHT
+    page.title = 'Моё первое приложение'
+    page.theme_mode = ft.ThemeMode.SYSTEM
 
-    text_hello = ft.Text(value='Hello Dolka')
+    greeting_history = []
+    history_text = ft.Text("История приветствий: ")
 
 
-    def text_name(_):
+    text_hello = ft.Text(value="Hello world!")
+
+
+    def text_name(e):
+
         name = name_input.value.strip()
+        now = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
 
         if name:
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            text_hello.value = f'{now} - Привет, {name}!'
             text_hello.color = None
-            text_hello.value = f"{current_time}-Привет, {name}!"
             name_input.value = None
+
+            greeting_history.append(name)
+            print(greeting_history)
+            history_text.value = 'История приветствий:\n' + '\n'.join(greeting_history)
+            
         else:
             text_hello.value = "Введите имя!"
             text_hello.color = ft.Colors.RED
-        page.update()
-        
-        
-
-    elevated_button = ft.ElevatedButton ('send', on_click=text_name, icon=ft.Icons.SEND, color=ft.Colors.RED, icon_color=ft.Colors.BLACK)
-    
-    name_input = ft.TextField(label='Введите что-то', on_submit=text_name)
 
     def thememode(_):
-        if page.theme_mode == ft.ThemeMode.DARK:
-            page.theme_mode = ft.ThemeMode.LIGHT
-        else:
+        
+        if page.theme_mode == ft.ThemeMode.LIGHT:
             page.theme_mode = ft.ThemeMode.DARK
+        else:
+            page.theme_mode = ft.ThemeMode.LIGHT
+                
 
     thememode_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_7, on_click=thememode)
-    
 
-    page.add(text_hello, name_input, elevated_button, thememode_button)
+    def clear_history(e):
+        print(greeting_history)
+        greeting_history.clear()
+        print(greeting_history)
+        history_text.value = "История приветствий: "
+
+    def sort_history (_):
+        greeting_history.sort()
+        history_text.value = 'История приветствий:\n' + '\n'.join(greeting_history)
+
+
+    sort_button = ft.ElevatedButton('Сортировать по алфавиту', on_click=sort_history, icon=ft.Icons.SORT_BY_ALPHA)
+
+    clear_button =ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
+
+    name_input = ft.TextField(label='Введите ваше имя', on_submit=text_name, expand=True)
+
+    elevated_button = ft.ElevatedButton('send', on_click=text_name, icon=ft.Icons.SEND)
+
+    main_object = ft.Row([name_input, elevated_button, thememode_button, clear_button, sort_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
+
+
+    page.add(text_hello, main_object, history_text) 
 
 ft.app(target=main) 
+
